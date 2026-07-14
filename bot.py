fb462e589bc6fd9a7d9439df79a0d8ad9f153493
securetykeyless-doto <securetykeyless@gmail.com>
2026-07-14 22:56:17 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -25,10 +25,10 @@ async def start_handler(message: types.Message):
         "Welcome to our service! 🚗\n\n"
         "We specialize in professional automotive solutions for European customers. "
         "Our process is transparent, secure, and fast.\n\n"
-        "1. Select your service.\n"
+        "1. Select your service.Hundai VIN to PIN\n"
         "2. Complete the secure cryptocurrency payment via MaxelPay.\n"
         "3. Provide your VIN number for processing.\n"
-        "4. Receive your access PIN-code instantly.\n\n"
+        "4. Receive your access PIN-code instantly.15-60 min.\n\n"
         "Press /pay to start your order."
     )
 
